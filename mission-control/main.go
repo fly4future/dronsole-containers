56bed202e23a1b3cb7dea67226fcdb031992461f
Esko Oramaa <esko.oramaa@solita.fi>
2021-02-10 13:52:55 +0200

    Allow CORS for mission-control

--- mission-control/main.go
+++ mission-control/main.go
@@ -27,6 +27,17 @@ func main() {
 	defer mqttClient.Disconnect(1000)
 	router := httprouter.New()
 	registerRoutes(router)
+	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Access-Control-Request-Method") != "" {
+			// Set CORS headers
+			header := w.Header()
+			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
+			header.Set("Access-Control-Allow-Origin", "*")
+		}
+
+		// Adjust status code to 204
+		w.WriteHeader(http.StatusNoContent)
+	})
 
 	listenMQTTEvents(mqttClient)
 
