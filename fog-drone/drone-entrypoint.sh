deffaa390779a444f25628683e00431b4967f53b
Olli-Antti Kivilahti <olli-antti.kivilahti@solita.fi>
2021-02-11 14:53:15 +0000

    fog-drone - document fog-drone entrypoint required ENV vars

--- fog-drone/drone-entrypoint.sh
+++ fog-drone/drone-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+test -z "$MQTT_BROKER_ADDRESS" && echo "MQTT_BROKER_ADDRESS is not defined!" && exit 201
+test -z "$DRONE_DEVICE_ID" && echo "DRONE_DEVICE_ID is not defined!" && exit 202
+test -z "$DRONE_IDENTITY_KEY" && echo "DRONE_IDENTITY_KEY is not defined!" && exit 203
+
 mkdir /enclave
 echo "$DRONE_IDENTITY_KEY" > /enclave/rsa_private.pem
 
