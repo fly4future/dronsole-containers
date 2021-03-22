17078719f9295b2bfa499ce98461470b45ecaf75
Esko Oramaa <esko.oramaa@solita.fi>
2021-03-22 20:40:30 +0200

    Use new videonode parameters

--- fog-drone/run-ros2.sh
+++ fog-drone/run-ros2.sh
@@ -8,6 +8,6 @@ ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >/fog-drone/mav_ctrl_out.log 2>
 echo "Start Micrortps_agent"
 micrortps_agent -t UDP -n "$DRONE_DEVICE_ID" >/fog-drone/urtps_out.log 2>/fog-drone/urtps_err.log &
 echo "Start videonode"
-videonode -device_id "$DRONE_DEVICE_ID" >/fog-drone/videonode_out.log 2>/fog-drone/videonode_err.log &
+videonode -stream-address "rtsp://$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID" >/fog-drone/videonode_out.log 2>/fog-drone/videonode_err.log &
 echo "Start Communication link"
 communication_link -device_id "$DRONE_DEVICE_ID" -mqtt_broker "$MQTT_BROKER_ADDRESS"
