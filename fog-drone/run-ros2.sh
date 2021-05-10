deb03eba84416e2f7f10a8a088505401f6b547fe
Esko Oramaa <esko.oramaa@solita.fi>
2021-05-10 15:39:24 +0300

    Utilize gst-launch directly for video streaming

--- fog-drone/run-ros2.sh
+++ fog-drone/run-ros2.sh
@@ -7,8 +7,8 @@ echo "Start Mavlink control"
 ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >/fog-drone/mav_ctrl_out.log 2>/fog-drone/mav_ctrl_err.log &
 echo "Start Micrortps_agent"
 micrortps_agent -t UDP -n "$DRONE_DEVICE_ID" >/fog-drone/urtps_out.log 2>/fog-drone/urtps_err.log &
-echo "Start videonode"
-videonode -stream-address "rtsp://$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID" >/fog-drone/videonode_out.log 2>/fog-drone/videonode_err.log &
+echo "Start video"
+gst-launch-1.0 udpsrc port=5600 ! "application/x-rtp" ! rtph264depay ! queue ! video/x-h264 ! rtspclientsink name=sink protocols=tcp location="rtsp://$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID" tls-validation-flags=generic-error >/fog-drone/video_out.log 2>/fog-drone/video_err.log &
 if [ "$RECORD_MISSION_DATA" = true ]; then
     echo "Start mission data recorder"
     mission-data-recorder \
